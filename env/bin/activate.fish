28c46c22df24c3db31819a6d6bca3e84d699160a
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-09-24 12:03:51 -0300

    vamos ver se funciona

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jvpazotti/Desktop/env"
+set -gx VIRTUAL_ENV "/Users/jvpazotti/OneDrive - Insper - Institudo de Ensino e Pesquisa/4-Sem/Tec_Web/TecWeb-P1B/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
