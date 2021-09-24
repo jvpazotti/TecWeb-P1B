360b36318717068e966dbabbb865d27f71345149
jvpazotti <62957238+jvpazotti@users.noreply.github.com>
2021-09-24 12:55:11 -0300

    Heroku done

--- notes/models.py
+++ notes/models.py
@@ -15,7 +15,7 @@ class Tag(models.Model):
 class Note(models.Model):
     title = models.CharField(max_length=200, null=True)
     content=models.TextField(null=True)
-    tag=models.ForeignKey(to=Tag ,on_delete=models.CASCADE,null=True)
+    tag=models.ForeignKey(to=Tag ,on_delete=models.SET_NULL,null=True)
 
     def __str__(self):
         return f"{self.id}.{self.title}"
